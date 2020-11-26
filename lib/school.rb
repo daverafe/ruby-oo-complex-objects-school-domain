ae37306e79d3b37ce602b53b3e089cc17f6055c5
David Rafe <david.rafe@gmail.com>
2020-11-25 21:27:23 -0500

    commit tree dcce53860ad98330a47926b9fe154f9ed0a07009

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require "pry"
+
+class School
+    attr_accessor :name, :roster 
+    def initialize(name, roster = {})
+        @name = name 
+        @roster = roster 
+    end
+
+    def add_student(name, grade)
+       if @roster.include?(grade) 
+        @roster[grade] << name 
+       else
+        @roster[grade] = [name] 
+        end
+    end
+
+    def grade(grade)
+        @roster[grade] 
+    end
+
+    def sort
+        @roster.each do |grades, students|
+            students.sort!
+        end
+    end
+end
